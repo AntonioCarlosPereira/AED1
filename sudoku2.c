548d1292bdb302d6bc0c2f937e4288befee1cb66
AntonioCarlosPereira <4nt.556@gmail.com>
2024-10-29 20:21:54 -0300

    Update sudoku2.c

--- sudoku2.c
+++ sudoku2.c
@@ -3,3 +3,23 @@
 //cria uma resposta final
 //esconda um numero x de inputs
 //verificação final
+rand 1-9 => matriz[1][1]
+mais[1][2] => rand 1-9 != matriz[1][1]
+==3 => rand 1-9 != ==1 && != 2
+
+matriz[2][1] => rand 1-9 != matriz[1][1]
+
+verifica a matriz resposta
+
+scf x numeros 
+for(x vezes)
+rand 1-9 =>num1 rand 1-9 => num2
+casa x da matriz[num1][num2]
+
+reorna a matriz com os numeros escondidos
+
+pede a resolução 
+
+resposta == matriz resposta
+
+tela de vitória 
